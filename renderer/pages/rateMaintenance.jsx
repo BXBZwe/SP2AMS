4ff2a391e865d4196bbf773aea1e03511b3b2c2c
Ahmad Yasi Faizi <yasifaizi10@gmail.com>
2023-12-22 14:10:32 +0700

    edit button added

--- renderer/pages/rateMaintenance.jsx
+++ renderer/pages/rateMaintenance.jsx
@@ -8,43 +8,7 @@ import Head from 'next/head'
 import { DataGrid } from '@mui/x-data-grid';
 
 export default function RateMaintenancePage() {
-  const status = [
-    { label: 'Booked' },
-    { label: 'Not Booked' },
-  ];
-  const type = [
-    { label: 'Studio'},
-    { label: 'Wifi'},
-    { label: 'Studio'},
-  ];
-  const rent = [
-    { label: 'Available' },
-    { label: 'Not Available' },
-  ]
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-  const columns = [
-    { field: 'id', headerName: 'Item ID', width: 130 },
-    { field: 'itemid', headerName: 'Item Name', width: 160 },
-    {
-      field: 'fee',
-      headerName: 'Fee',
-      type: 'number',
-      width: 100,
-    },
-    
-  ];
   
-  const rows = [
-    { id: 'A001', itemid: 'Car Parking', fee: 35 },
-    { id: 'A002', itemid: 'Furniture', fee: 42 },
-    { id: 'A003', itemid: 'Fridge', fee: 45 },
-    { id: 'A004', itemid: 'Air Con', fee: 16 },
-    { id: 'A005', itemid: 'Air Con', fee: 200 },
-    { id: 'A006', itemid: 'Air Con', fee: 150 },
-    { id: 'A007', itemid: 'Air Con', fee: 44 },
-    { id: 'A008', itemid: 'Air Con', fee: 36 },
-    { id: 'A009', itemid: 'Air Con', fee: 65 },
-  ];
   return (
     <>
     <Head>
