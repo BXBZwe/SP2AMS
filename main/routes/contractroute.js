347fbe6c909aed7686a83a455934bb7e677b855a
sawzwe26 <sawjohnwaiyan@gmail.com>
2024-03-08 16:11:04 +0700

    Contract Loading Added

--- main/routes/contractroute.js
+++ main/routes/contractroute.js
@@ -1,7 +1,24 @@
 import express from 'express';
-import { getTemplateFilePath, fetchTenantData, fillDocxTemplate, fetchContractDetail, updatePeriodOfStay } from '../controllers/contractcontroller';
+import { getTemplateFilePath, fetchTenantData, fillDocxTemplate, fetchContractDetail, updatePeriodOfStay,fetchSpecificContractDetail } from '../controllers/contractcontroller';
 
 const contractroute = express.Router();
+
+// contractroute.get('/createfilledcontract/:tenantId/:language', async (req, res) => {
+//     const { tenantId, language } = req.params;
+
+//     try {
+//         const templateFilePath = getTemplateFilePath(language);
+
+//         const tenantData = await fetchTenantData(parseInt(tenantId));
+
+//         const filledContractPath = await fillDocxTemplate(templateFilePath, tenantData, language);
+
+//         res.sendFile(filledContractPath);
+//     } catch (error) {
+//         res.status(500).send({ error: error.message });
+//     }
+// });
+
 
 contractroute.get('/createfilledcontract/:tenantId/:language', async (req, res) => {
     const { tenantId, language } = req.params;
@@ -13,13 +30,27 @@ contractroute.get('/createfilledcontract/:tenantId/:language', async (req, res) 
 
         const filledContractPath = await fillDocxTemplate(templateFilePath, tenantData, language);
 
-        res.sendFile(filledContractPath);
+        // Log the message just before sending the file
+        console.log("Document Created");
+
+        res.sendFile(filledContractPath, (err) => {
+            if (err) {
+                // In case there's an error sending the file, log it
+                console.error('Error sending the filled contract file:', err);
+            } else {
+                // Optionally, log that the file was successfully sent
+                console.log('Filled contract file sent successfully.');
+            }
+        });
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
 });
 
+
 contractroute.get('/getcontractdetails', fetchContractDetail);
+contractroute.get('/getspecificcontractdetails/:contractId', fetchSpecificContractDetail); 
+
 
 contractroute.put('/updatePeriodOfStay/:tenantId', async (req, res) => {
     const { tenantId } = req.params;
@@ -31,6 +62,8 @@ contractroute.put('/updatePeriodOfStay/:tenantId', async (req, res) => {
         res.status(500).send({ message: 'Error updating period of stay', error: error.message });
     }
 });
+
+
 
 
 export default contractroute;
